2be2664e0e03a4580e2678816973426b1fc59410
tomiwadoesux <141045581+tomiwadoesux@users.noreply.github.com>
2025-04-14 09:17:23 +0100

    third

--- components/LazyImageTrail.js
+++ components/LazyImageTrail.js
@@ -5,7 +5,7 @@ import { useState, Suspense, lazy } from "react";
 import { useInView } from "react-intersection-observer";
 
 
-const ImageTrail = lazy(() => import(".//ImageTrail"));
+const ImageTrail = lazy(() => import(".//Imagetrail"));
 
 const LazyImageTrail = ({ imageSrc }) => {
   const { ref, inView } = useInView({
