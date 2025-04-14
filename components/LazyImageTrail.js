159df1ba04f39f57efc5eb1b535bb5cfe77440d0
tomiwadoesux <141045581+tomiwadoesux@users.noreply.github.com>
2025-04-14 09:26:13 +0100

    Fix: correct import path for ImageTrail again

--- components/LazyImageTrail.js
+++ components/LazyImageTrail.js
@@ -5,7 +5,7 @@ import { useState, Suspense, lazy } from "react";
 import { useInView } from "react-intersection-observer";
 
 
-const ImageTrail = lazy(() => import(".//Imagetrail"));
+const ImageTrail = lazy(() => import("components/ImageTrail"));
 
 const LazyImageTrail = ({ imageSrc }) => {
   const { ref, inView } = useInView({
